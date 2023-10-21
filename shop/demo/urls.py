6aaef2e55b029c3ab83053d0b958fa61f9005dfd
guv <jurapro@yandex.ru>
2023-10-21 16:00:32 +0700

    исправления верстки и пр

--- shop/demo/urls.py
+++ shop/demo/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls.static import static
-from django.contrib import admin
+
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-
+    path('login/', auth_views.LoginView.as_view(), name='login'),
 ]
