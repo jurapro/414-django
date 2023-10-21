f75afb8acc2ace0ad94e4c4f19ad07c144702050
guv <jurapro@yandex.ru>
2023-10-21 16:23:42 +0700

    регистрация фулл

--- shop/demo/urls.py
+++ shop/demo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
+from .views import RegisterView
+
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
--- shop/demo/views.py
+++ shop/demo/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
+from .forms import RegisterUserForm
+
+
 # Create your views here.
 class RegisterView(CreateView):
     template_name = 'registration/register.html'
