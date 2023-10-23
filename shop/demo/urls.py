4215a27f0afe0827d2f74732d2e663496592c097
guv <jurapro@yandex.ru>
2023-10-23 17:02:53 +0700

    urls.py routes

--- shop/demo/urls.py
+++ shop/demo/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
-from .views import RegisterView
+from .views import *
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
+
+    path('', catalog, name='catalog'),
+    path('about', about, name='about'),
+    path('contact', contact, name='contact'),
+    path('product/<pk>', product, name='product'),
+
+    path('cart', cart, name='cart'),
+    path('checkout', checkout, name='checkout'),
+    path('orders', OrderListView.as_view(), name='orders'),
 ]
